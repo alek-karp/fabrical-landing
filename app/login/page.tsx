db8f10fe1a1af1a91b036b5c6af6db71ed354f3f
Alek Karp <aleksander.karp2000@gmail.com>
2026-06-29 16:25:47 -0700

    feat(login): add back button

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,25 @@
-import { LoginForm } from "@/components/login-form"
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
+import { LoginForm } from "@/components/login-form";
+import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+    <div className="relative flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <Button
+        asChild
+        variant="ghost"
+        className="absolute left-4 top-4 rounded-none md:left-6 md:top-6"
+      >
+        <Link href="/">
+          <ArrowLeft />
+          Back
+        </Link>
+      </Button>
       <div className="w-full max-w-sm md:max-w-4xl">
         <LoginForm />
       </div>
     </div>
-  )
+  );
 }
